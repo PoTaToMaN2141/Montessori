90064160ce499e2a5eb8d10399f7fa03ccdca1ad
Iain Davis (RIT Student) <icd1636@ad.rit.edu>
2019-11-15 21:31:48 -0500

    Test Junk

---
+++ Assets/ScriptableObjects/BounceObject.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "BounceObject")]
+public class BounceObject : ScriptableObject
+{
+    public float bounceAmount;
+}
---
+++ Assets/Scripts/BoxFall.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxFall : MonoBehaviour
+{
+    public Collider collider;
+    public Rigidbody rigidBody;
+    public BounceObject bounceDetails;
+    private float bounceMultiplier = 1;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bounceMultiplier + Input.mouseScrollDelta.normalized.y/10 >= 0)
+        {
+            bounceMultiplier += Input.mouseScrollDelta.normalized.y/10;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.SetBool("Big", true);
+        }
+        else
+        {
+            animator.SetBool("Big", false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rigidBody.velocity = -1 * rigidBody.velocity * bounceDetails.bounceAmount * bounceMultiplier;
+    }
+}
